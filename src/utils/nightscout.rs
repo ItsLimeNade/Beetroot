049df6e759a632c9301b9c9e16ee48744316a092
LimeNade <alexandre.zarkua@gmail.com>
2025-09-23 00:01:01 +0200

    Add documentation to nightscout.rs
    Added doc comments to most of the nightscout.rs file.

--- src/utils/nightscout.rs
+++ src/utils/nightscout.rs
@@ -3,6 +3,9 @@ use serde::Deserialize;
 use thiserror::Error;
 
 #[derive(Debug)]
+/// Represents a Nightscout client for interacting with the Nightscout API.
+/// 
+/// This struct holds an HTTP client used to send requests to the Nightscout service.
 pub struct Nightscout {
     http_client: Client
 }
@@ -10,10 +13,17 @@ pub struct Nightscout {
 #[derive(Debug, Error)]
 pub enum NightscoutError {
     #[error("Network error: {0}")]
+    /// Represents errors that occur during network requests, such as connection failures,
+    /// timeouts, or invalid responses, as reported by the `reqwest` HTTP client library.
     Network(#[from] reqwest::Error),
+    /// Represents an error that occurs when no reaings are read when fetching Nightscout. 
     #[error("No entries found")]
     NoEntries,
     #[error("Invalid URL: {0}")]
+    /// Represents an error that occurs when parsing a URL using the `url` crate.
+    /// 
+    /// This variant is generated when a URL string cannot be successfully parsed.
+    /// See [`url::ParseError`](https://docs.rs/url/latest/url/enum.ParseError.html) for more details.
     Url(#[from] url::ParseError),
 }
 
@@ -42,6 +52,12 @@ pub struct NightscoutRequestOptions {
 }
 
 impl NightscoutRequestOptions {
+    /// Sets the ammount of entries that will be fetched from Nightscout.
+    /// 
+    /// ```
+    /// let options = NightscoutRequestOptions::default()
+    /// .count(5);
+    /// ```
     pub fn count(mut self, count: u8) -> Self {
         self.count = Some(count);
         self
@@ -49,15 +65,19 @@ impl NightscoutRequestOptions {
 }
 
 impl Nightscout {
+    /// Creates a new instance of `Nightscout` with a default HTTP client.
     pub fn new() -> Self {
         Nightscout { http_client: Client::new() }
     }
 
+    /// Returns an `Entry` if available, or a `NightscoutError::NoEntries` if no entries are found.
     pub async fn get_entry(&self, base_url: &str) -> Result<Entry, NightscoutError> {
         let entries = self.get_entries(base_url, NightscoutRequestOptions::default()).await?;
         entries.first().cloned().ok_or(NightscoutError::NoEntries)
     }
 
+    /// The number of entries returned is determined by `options.count`, defaulting to 1 if not specified.
+    /// Returns a vector of `Entry` objects or a `NightscoutError` on failure.
     pub async fn get_entries(&self, base_url: &str, options: NightscoutRequestOptions) -> Result<Vec<Entry>, NightscoutError> {
         let count: u8 = options.count.unwrap_or(1);
 
